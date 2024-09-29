d3da5ac08c2ab66a22d785a8ad2e7982b97ebb88
Go-Champ <155908806+Go-Champ@users.noreply.github.com>
2024-09-29 11:43:18 +0530

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# this is the main file to create the individuals and run the simulation 
 
+class population:
+    def __init__(self, freq_A, freq_a):
+        allele_freq_A = freq_A
+        allele_freq_a = freq_a
+        genotype_freq_AA = allele_freq_A*allele_freq_A
+        genotype_freq_Aa = 2*allele_freq_A*allele_freq_a
+        genotype_freq_aa = allele_freq_a*allele_freq_a
+
+    def show_freq(self):
+        print (genotype_freq_AA)
+
+population(0.7, 0.3)
