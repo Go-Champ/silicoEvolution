f04d2f7485e7bc575571d6db25e16515cedf7181
Go-Champ <155908806+Go-Champ@users.noreply.github.com>
2024-09-29 11:51:02 +0530

    fresh commit

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ class population:
         genotype_freq_AA = allele_freq_A*allele_freq_A
         genotype_freq_Aa = 2*allele_freq_A*allele_freq_a
         genotype_freq_aa = allele_freq_a*allele_freq_a
-
-    def show_freq(self):
-        print (genotype_freq_AA)
-
-population(0.7, 0.3)
+        print("A -> ", allele_freq_A, '/n',
+            "a -> ", allele_freq_a, '/n',
+            "Genotype AA -> ", genotype_freq_AA, '/n,'
+            "Genotype Aa -> ", genotype_freq_Aa, '/n,'
+            "Genotype aa -> ", genotype_freq_aa)
+        
+population (0.7, 0.3)
